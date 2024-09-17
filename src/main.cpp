61d3fedb95127d5c9de34fa56bed1b864875137c
Patrick Meier <pat.meier.rick@gmail.com>
2024-09-17 18:48:33 +0200

    Initial commit - Adding PlatformIO project

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+// Definiere den Pin der eingebauten LED (je nach Board-Variante kann das 2 oder 13 sein)
+#define LED_BUILTIN 13
+
+void setup() {
+  // Setze den LED-Pin als Ausgang
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // Schalte die LED ein
+  digitalWrite(LED_BUILTIN, HIGH);
+  // Warte 1000 Millisekunden (1 Sekunde)
+  delay(1000);
+  // Schalte die LED aus
+  digitalWrite(LED_BUILTIN, LOW);
+  // Warte 1000 Millisekunden (1 Sekunde)
+  delay(1000);
+}
